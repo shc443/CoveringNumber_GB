fa55853bbc0d48f1227da7ffe15985cb55398aad
shc443 <shc443@nyu.edu>
2024-07-08 16:46:11 -0400

    fixed model codes

--- net_model.py
+++ net_model.py
@@ -8,156 +8,186 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 import torch.optim as optim
 
 import matplotlib.pyplot as plt
-
 import numpy as np
 import pandas as pd
 import wandb
 
-###
 import seaborn as sns
+from math import prod
 # import pickle5 as pickale
 import pickle
 
 
 class Net(nn.Module):
-    def __init__(self, widths, nonlin=F.relu, transfer = False, loss = None):
+
+    """(Deep | AccNets | Shallow) Neural Network Model"""
+
+    def __init__(self, widths, nonlin=F.relu, transfer=False, loss='L1', test_loss=None, prof=True, wandb=False):
+        """
+        :param widths(list of ints): widths of each layer in the network; e.g. [d_in, w, d_out] ~ [15, 500, 20] for a ShallowNet
+        :param nonlin: nonlinear activation function; F.relu, F.leaky_relu, F.sigmoid, F.tanh
+        :param transfer (bool, optional): whether to use transfer learning method:
+        :param loss(str): type of loss function used during training; 'L1' or 'L2', all the others will be considered as nuclear norm
+        :param test_loss(str): type of loss function used for validation (test); 'L1' or 'L2', all the others will be considered as nuclear norm
+        :param prof(bool): whether to use Prof. Jacot's L1 definition
+        """
         super(Net, self).__init__()
-        self.widths=widths
-        self.nonlin=nonlin
-        self.linears = nn.ModuleList([nn.Linear(w1, w2) for w1,w2 in zip(widths[:-1],widths[1:])])
+        self.widths = widths
+        self.nonlin = nonlin
+        self.linears = nn.ModuleList([nn.Linear(w1, w2) for w1, w2 in zip(widths[:-1], widths[1:])])
         self.alpha = [0 for _ in self.widths]
         self.pre_alpha = [0 for _ in self.widths]
         self.opt = None
         self.transfer = transfer
-        self.loss = loss #only for non-transfer
+        self.loss = loss
+        self.prof = prof
+        self.test_loss = test_loss
+        self.wandb = wandb
 
     def forward(self, x):
         self.alpha[0] = x
         for i, lin in enumerate(self.linears):
-            self.pre_alpha[i+1] = lin(self.alpha[i])
-            self.alpha[i+1] = self.nonlin(self.pre_alpha[i+1])
-        return self.pre_alpha[len(self.widths)-1]
+            self.pre_alpha[i + 1] = lin(self.alpha[i])
+            self.alpha[i + 1] = self.nonlin(self.pre_alpha[i + 1])
+        return self.pre_alpha[len(self.widths) - 1]
 
-    def train(self, X_train, Y_train, X_test, Y_test, lr=0.5, weight_decay = 0.0, epochs = 2000, num_batches=1):
+    def train(self, X_train, Y_train, X_test, Y_test, lr=0.5, weight_decay=0.0, epochs=2000, num_batches=1):
         N = X_train.shape[0] // num_batches
         if self.opt == None:
-            self.opt = optim.Adam(self.parameters(),lr=lr,weight_decay=weight_decay)
-        
+            self.opt = optim.Adam(self.parameters(), lr=lr, weight_decay=weight_decay)
+
         for g in self.opt.param_groups:
             g['lr'] = lr
             g['weight_decay'] = weight_decay
-        
+
         for t in range(epochs):
             for ib in range(num_batches):
 
                 self.opt.zero_grad()
-                
+
                 if self.transfer:
-                    N_half = N//2
-                    X1 = X_train[N * ib : N * ib + N_half]
-                    X2 = X_train[N * ib + N_half : N * (ib + 1)]
+                    N_half = N // 2
+                    X1 = X_train[N * ib: N * ib + N_half]
+                    X2 = X_train[N * ib + N_half: N * (ib + 1)]
 
                     Y1 = self(X1)
                     Y2 = self(X2)
-                    
-                    cost1 = ((Y1[:,:Y1.shape[1]//2]
-                              - Y_train[N * ib : N * ib + N_half, :Y1.shape[1]//2])**2).mean()
-                    ###Todo" typo change cost2 to second half 
-                    cost2 = ((Y2[:,Y2.shape[1]//2:]
-                              - Y_train[N * ib + N_half : N * (ib + 1), Y2.shape[1]//2:])**2).mean()
+
+                    cost1 = ((Y1[:, :Y1.shape[1] // 2]
+                              - Y_train[N * ib: N * ib + N_half, :Y1.shape[1] // 2]) ** 2).mean()
+                    ###Todo" typo change cost2 to second half
+                    cost2 = ((Y2[:, Y2.shape[1] // 2:]
+                              - Y_train[N * ib + N_half: N * (ib + 1), Y2.shape[1] // 2:]) ** 2).mean()
                     cost = cost1 + cost2
-                    
+
                 else:
-                    YY_train = self(X_train[N * ib:N*(ib+1)])
-                    if self.loss == None:
-                        cost = ((YY_train -  Y_train[N * ib:N*(ib+1)])**2).mean()
-                    elif self.loss == 'L1':
-                        # cost = torch.linalg.norm(YY_train -  Y_train[N * ib:N*(ib+1)], dim=1, ord=1)
-                        cost = torch.abs((YY_train -  Y_train[N * ib:N*(ib+1)])).mean()
+                    YY_train = self(X_train[N * ib:N * (ib + 1)])
+                    if self.loss == 'L2':
+                        cost = nn.MSELoss(reduction='mean')(YY_train, Y_train[N * ib:N * (ib + 1)])
+                    elif (self.loss == 'L1') and (not self.prof):
+                        cost = nn.L1Loss(reduction='mean')(YY_train, Y_train[N * ib:N * (ib + 1)])
+                    elif (self.loss == 'L1') and (self.prof):
+                        cost = (torch.sum((YY_train - Y_train[N * ib:N * (ib + 1)]) ** 2, axis=0) ** 0.5).mean()
                     else:
-                        cost = (YY_train -  Y_train[N * ib:N*(ib+1)]).norm(p='nuc')
-                    
+                        cost = (YY_train - Y_train[N * ib:N * (ib + 1)]).norm(p='nuc')
+
                 cost.backward()
                 self.opt.step()
 
-            if t % 50 == 0:
-                if self.transfer:
-                    wandb.log({"cost": cost.item(),"cost1": cost1.item(),"cost2": cost2.item(), 
-                           "test_cost": ((self(X_test) -  Y_test)**2).mean().item(), 
+
+        if self.test_loss == 'L2':
+            test_cost = nn.MSELoss(reduction='mean')(self(X_test), Y_test)
+        elif (self.test_loss == 'L1') and (not self.prof):
+            test_cost = nn.L1Loss(reduction='mean')(self(X_test), Y_test)
+        elif (self.test_loss == 'L1') and (self.prof):
+            test_cost = (torch.sum((self(X_test) - Y_test) ** 2, axis=0) ** 0.5).mean()
+        else:
+            test_cost = (self(X_test) - Y_test).norm(p='nuc')
+
+        if t % 50 == 0:
+            if self.transfer:
+                wandb.log({"cost": cost.item(), "cost1": cost1.item(), "cost2": cost2.item(),
+                           "test_cost": test_cost.item(),
                            "norm": self.norm() / self.depth()})
-                else: 
-                    wandb.log({"cost": cost.item(), 
-                               "test_cost": ((self(X_test) -  Y_test)**2).mean().item(), 
-                               "norm": self.norm() / self.depth()})
-#                 print(cost.item(), ((self(X_test) -  Y_test)**2).mean().item(), self.norm() / self.depth())
-        return cost.item(), ((self(X_test) -  Y_test)**2).mean().item(), self.norm() / self.depth()
+            else:
+                wandb.log({"cost": cost.item(),
+                           "test_cost": test_cost.item(),
+                           "norm": self.norm() / self.depth()})
 
-#     def train_transfer(self, X_train, Y_train, X_test, Y_test, lr=0.5, weight_decay = 0.0, epochs = 2000, num_batches=1):
-#         N = X_train.shape[0] // num_batches
-#         N_half = N//2
-        
-#         if self.opt == None:
-#             self.opt = optim.Adam(self.parameters(),lr=lr,weight_decay=weight_decay)
-        
-#         for g in self.opt.param_groups:
-#             g['lr'] = lr
-#             g['weight_decay'] = weight_decay
-        
-#         for t in range(epochs):
-#             for ib in range(num_batches):
 
-#                 self.opt.zero_grad()
-                
-#                 X1 = X_train[N * ib : N * ib + N_half]
-#                 X2 = X_train[N * ib + N_half : N * (ib + 1)]
-                
-#                 Y1 = self(X1)
-#                 Y2 = self(X2)
-                
-# #                 cost = ((YY_train -  Y_train[N * ib:N*(ib+1)])**2).mean()
-#                 cost1 = ((Y1[:,:Y1.shape[1]//2]-Y_train[:,:Y1.shape[1]//2])**2).mean()
-#                 cost2 = ((Y2[:,Y2.shape[1]//2:]-Y_train[:,Y2.shape[1]//2:])**2).mean()
-#                 cost = cost1 + cost2
-#                 cost.backward()
-#                 self.opt.step()
+        return cost.item(), test_cost.item(), self.norm() / self.depth()
+    # Update Loss function : L1 norm along datapoint sum((self(X_test) -  Y_test))**2, axis = row)**0.5.mean()
 
-#             if t % 50 == 0:
-#                 wandb.log({"cost": cost.item(), 
-#                            "test_cost": ((self(X_test) -  Y_test)**2).mean().item(), 
-#                            "norm": self.norm() / self.depth()})
-# #                 print(cost.item(), ((self(X_test) -  Y_test)**2).mean().item(), self.norm() / self.depth())
-#         return cost.item(), ((self(X_test) -  Y_test)**2).mean().item(), self.norm() / self.depth()
-    
     def nonlin_impact(self):
         nonlin_impact = np.zeros([len(self.alpha)])
-        for i in range(1,len(self.alpha)):
-            nonlin_impact[i] = ((self.alpha[i] - self.pre_alpha[i])**2).sum().item() / (self.pre_alpha[i]**2).sum().item()
+        for i in range(1, len(self.alpha)):
+            nonlin_impact[i] = ((self.alpha[i] - self.pre_alpha[i]) ** 2).sum().item() / (
+                        self.pre_alpha[i] ** 2).sum().item()
         return nonlin_impact
 
     def eigs_alpha(self, num_eig=10):
-#         eigs = np.zeros([len(self.alpha),num_eig])
         eigs = []
-        
-        for i,alpha in enumerate(self.alpha):
-            S = torch.svd(alpha - 0*alpha.mean(0))[1].detach().cpu().numpy()
-            if i==len(self.alpha)-1:
-                S = torch.svd(self.pre_alpha[i] - 0*self.pre_alpha[i].mean(0))[1].detach().cpu().numpy()
+
+        for i, alpha in enumerate(self.alpha):
+            S = torch.svd(alpha - 0 * alpha.mean(0))[1].detach().cpu().numpy()
+            if i == len(self.alpha) - 1:
+                S = torch.svd(self.pre_alpha[i] - 0 * self.pre_alpha[i].mean(0))[1].detach().cpu().numpy()
             eigs.append(S[:num_eig])
         return eigs
-    
+
     def eigs_W(self, num_eig=10):
-#         eigs = np.zeros([len(self.linears),num_eig])
         eigs = []
-        
-        for i,lin in enumerate(self.linears):
+
+        for i, lin in enumerate(self.linears):
             S = torch.svd(lin.weight)[1].detach().cpu().numpy()
-#             eigs[i, :] = S[:num_eig]
             eigs.append(S[:num_eig])
         return eigs
-    
+
     def norm(self):
-        return sum([(p**2).sum().item() for p in self.parameters()])
-    
+        return sum([(p ** 2).sum().item() for p in self.parameters()])
+
     def depth(self):
-        return len(self.widths)-1
-    
+        return len(self.widths) - 1
+
+    def norms(self):
+        return [0.5 * torch.norm(self.linears[0].weight, p='fro') ** 2] + \
+            [0.5 * torch.norm(self.linears[idx].weight, p='nuc') ** 2 + 0.5 * torch.norm(self.linears[idx - 1].weight,
+                                                                                         p='nuc') ** 2 for idx in
+             range(len(self.linears))[2::2]] + \
+            [0.5 * torch.norm(self.linears[-1].weight, p='fro') ** 2]
+
+    def Lip_OP(self):
+        return torch.Tensor(
+            [(torch.linalg.matrix_norm(linear.weight, ord=2)).item() for linear in self.linears]).reshape(-1, 2).prod(
+            axis=1)
+
+    def ranks(self, atol=0.1, rtol=0.1):
+        return ([self.widths[0]]
+                + [max(torch.linalg.matrix_rank(self.linears[idx].weight, atol=atol, rtol=rtol),
+                       torch.linalg.matrix_rank(self.linears[idx - 1].weight, atol=atol, rtol=rtol)).item() for idx in
+                   range(len(self.linears))[2::2]]
+                + [self.widths[-1]])
+
+    def stable_ranks(self):
+        return ([self.widths[0]]
+                + [(torch.norm(linear.weight) ** 2
+                    / torch.linalg.matrix_norm(linear.weight, ord=2) ** 2).item()
+                   for linear in self.linears[2::2]]
+                + [self.widths[-1]])
+
+    def complexity1(self, norms, lips, dims):
+        return prod(lips) * sum([n / l * (dm + dp) ** 0.5 for n, l, dm, dp in zip(norms, lips, dims[:-1], dims[1:])])
+
+    def complexities(self, X_train):
+        norms = self.norms()
+        Lip_OPs = self.Lip_OP()
+
+        # Compute complexity; complexity2 is based on stable ranks
+        complexities1 = self.complexity1(norms, Lip_OPs, self.ranks()).item()
+        complexities2 = self.complexity1(norms, Lip_OPs, self.stable_ranks()).item()
+
+        # Compute variable terms in front of complexities
+        rho = 1  # for L1
+        b = max(torch.linalg.norm(torch.Tensor(X_train), dim=1))  # size of input ball
+        constants = rho * b
+        return constants * complexities1 / X_train.shape[0] ** 0.5, constants * complexities2 / X_train.shape[0] ** 0.5
--- trainNet.py
+++ trainNet.py
@@ -22,32 +22,57 @@ import pickle
 from net_model import Net
 from util import Helper
 
+
 class trainNet(Helper):
-    
-    def __init__(self, 
-                 full_N = 52500, 
-                 d_in = 15, 
-                 d_mid = 3, 
-                 d_out = 20, 
-                 w = [500], 
-                 Ns = [10000],
-                 test_N = 2500,
-                 trial = 3, 
-                 num_eig = 15,
-                 L=12, 
-                 transfer = False, 
-                 loss = None, 
-                 w8_dk = 0,
-                 requires_iNout = True,
-                 path2data = None,
-                 prjt_name = None):
-        
-        super().__init__(os.path.abspath(os.getcwd())+'/config.json')
-        
+    '''Tranier Module for numerical simulations'''
+
+    def __init__(self,
+                 full_N=52500,
+                 d_in=15,
+                 d_mid=3,
+                 d_out=20,
+                 w=[500],
+                 Ns=[10000],
+                 test_N=2500,
+                 trial=3,
+                 num_eig=15,
+                 L=12,
+                 transfer=False,
+                 loss=None,
+                 test_loss=None,
+                 prof=False,
+                 w8_dk=0,
+                 requires_iNout=True,
+                 path2data=None,
+                 prjt_name=None,
+                 wandb=False):
+        '''
+        :param full_N (int): total number of samples; in our paper, full_N is fixed as 52500 (50000 Training + 2500 Test)
+        :param d_in (int): input dimension of dataset; 15
+        :param d_mid (int): dimension of middle layers; 500 for DNN and AccNets or 50000 for Shallow net
+        :param d_out (int): dimension of output layers; 20
+        :param w (list of ints): list of widths dimensions; [15, 500, 500, ... , 500, 20] for DNN
+        :param Ns (list of ints): list of input dataset size; sudo-logarithmic increment from 100 to full_n
+        :param test_N (int): number of test samples; 2500
+        :param trial (int): number of trials; 3
+        :param num_eig (int): number of eigenvalues/eigenfunctions to find; 15
+        :param L (int): depth of networks; 12
+        :param transfer (bool, optional): whether to use transfer learning methods; False for this paper
+        :param loss (str): loss function used during training; "L1" or "L2", all the others are considered as nuclear norm
+        :param test_loss (str): loss function used during testing; "L1" or "L2", all the others are considered as nuclear norm
+        :param prof (bool): whether to use Prof. Jacot's L1 code
+        :param w8_dk (float): weight decay rate
+        :param requires_iNout (bool, str, optional): depreciated part; set to be 'acc'
+        :param path2data (str): directory of dataset
+        :param prjt_name (str, optional): name of project for WanDB;
+        :param wandb (bool): whether to use WanDB for monintoring model performance; please set this as `False`
+        '''
+        super().__init__(os.path.abspath(os.getcwd()) + '/config.json')
+
         self.full_N = full_N
-        self.d_in   = d_in 
-        self.d_mid  = d_mid
-        self.d_out  = d_out
+        self.d_in = d_in
+        self.d_mid = d_mid
+        self.d_out = d_out
 
         self.w = w
         self.trial = trial
@@ -56,109 +81,95 @@ class trainNet(Helper):
         self.Ns = Ns
         self.test_N = test_N
         self.num_Ns = len(self.Ns)
-#         self.deep = deep
+        #         self.deep = deep
         self.L = L
         self.w8_dk = w8_dk
         self.transfer = transfer
         self.loss = loss
-#         self.increment = increment
-        
-#         if not increment:
-#             self.num_Ns = 1
-            
+        self.test_loss = test_loss
+        self.prof = prof
+
         self.path2data = path2data
         self.prjt_name = prjt_name
+        self.wandb = wandb
+
         if requires_iNout == True:
-            self.widths = [d_in] + L*w + [d_out]   
-        elif requires_iNout == 'acc': #accodian structure; w is already well defined 
+            self.widths = [d_in] + L * w + [d_out]
+        elif requires_iNout == 'acc':  # accodian structure; w is already well defined
             self.widths = [d_in] + w + [d_out]
         else:
-            self.widths = L*w
-        
+            self.widths = L * w
+
     def wandb_setup(self, num_data, trial):
         os.system("wandb login")
         wandb.init(project=self.prjt_name, entity="kchoi1230")
-        wandb.run.name = 'testing_run_50k_{N}_{t}'.format(N = num_data, t = trial)
+        wandb.run.name = 'testing_run_50k_{N}_{t}'.format(N=num_data, t=trial)
         wandb.run.save()
-        
+
     def train_setup(self):
         self.train_costs = np.zeros([self.num_Ns, self.trial])
         self.test_costs = np.zeros([self.num_Ns, self.trial])
         self.norms = np.zeros([self.num_Ns, self.trial])
-#         self.eig_Ws = np.zeros([self.num_Ns, self.trial, len(self.widths)-1, self.num_eig])
-#         self.eig_alphas = np.zeros([self.num_Ns, self.trial, len(self.widths), self.num_eig])
+        #         self.eig_Ws = np.zeros([self.num_Ns, self.trial, len(self.widths)-1, self.num_eig])
+        #         self.eig_alphas = np.zeros([self.num_Ns, self.trial, len(self.widths), self.num_eig])
         self.eig_Ws = []
         self.eig_alphas = []
-        
+        self.complexity = []
+
     def load_dataset(self):
 
-        self.X = torch.Tensor(pd.read_pickle(self.X_path+self.path2data)).cuda()
-        self.Y = torch.Tensor(pd.read_pickle(self.Y_path+self.path2data)).cuda()
-        
+        self.X = torch.Tensor(pd.read_pickle(self.X_path + self.path2data)).cuda()
+        self.Y = torch.Tensor(pd.read_pickle(self.Y_path + self.path2data)).cuda()
+
     def train_net(self, iN, N, t):
-        self.net = Net(self.widths, transfer = self.transfer, loss = self.loss).cuda()
-        
-        self.net.train(self.X[:N],self.Y[:N],self.X[N:N+self.test_N],
-                       self.Y[N:N+self.test_N],lr=1.5 * 0.001, 
-                       weight_decay = self.w8_dk, 
-                       epochs = 1200, num_batches=5)
-        
-        self.net.train(self.X[:N],self.Y[:N],self.X[N:N+self.test_N],
-                       self.Y[N:N+self.test_N],lr=0.4 * 0.001, 
-                       weight_decay = self.w8_dk + 0.002, 
-                       epochs = 1200, num_batches=5)
-        
-        train_cost, test_cost, norm = self.net.train(self.X[:N],self.Y[:N],self.X[N:N+self.test_N],
-                                                     self.Y[N:N+self.test_N],lr=0.1 * 0.001
-                                                    , weight_decay = self.w8_dk + 0.0005, epochs = 1200, num_batches=5)
-        
-        self.train_costs[iN,t] = train_cost
-        self.test_costs[iN,t] = test_cost
-        self.norms[iN,t] = norm
-#         self.eig_Ws[iN,t,:,:] = self.net.eigs_W(self.num_eig)
-#         self.eig_alphas[iN,t,:,:] = self.net.eigs_alpha(self.num_eig)
-        self.eig_Ws.append(self.net.eigs_W(self.num_eig))
-        self.eig_alphas.append(self.net.eigs_alpha(self.num_eig))
-        
+        self.net = Net(self.widths, transfer=self.transfer, loss=self.loss, test_loss=self.test_loss, prof=self.prof,
+                       wandb=self.wandb).cuda()
+
+        self.net.train(self.X[:N], self.Y[:N], self.X[N:N + self.test_N], self.Y[N:N + self.test_N], lr=1.5 * 0.001,
+                       weight_decay=self.w8_dk, epochs=1200, num_batches=5)
+        self.net.train(self.X[:N], self.Y[:N], self.X[N:N + self.test_N], self.Y[N:N + self.test_N], lr=0.4 * 0.001,
+                       weight_decay=self.w8_dk + 0.002, epochs=1200, num_batches=5)
+        train_cost, test_cost, norm = self.net.train(self.X[:N], self.Y[:N], self.X[N:N + self.test_N],
+                                                     self.Y[N:N + self.test_N], lr=0.1 * 0.001
+                                                     , weight_decay=self.w8_dk + 0.0005, epochs=1200, num_batches=5)
+
+        self.train_costs[iN, t] = train_cost
+        self.test_costs[iN, t] = test_cost
+        self.norms[iN, t] = norm
+        #         self.eig_Ws[iN,t,:,:] = self.net.eigs_W(self.num_eig)
+        #         self.eig_alphas[iN,t,:,:] = self.net.eigs_alpha(self.num_eig)
+        # self.eig_Ws.append(self.net.eigs_W(self.num_eig))
+        # self.eig_alphas.append(self.net.eigs_alpha(self.num_eig))
+        self.complexity.append(self.net.complexities(self.X[:N]))
+
     def train_loop(self):
         for iN, N in enumerate(self.Ns):
             for t in range(self.trial):
                 self.wandb_setup(N, t)
                 self.train_net(iN, N, t)
                 wandb.finish()
-        
-#         else:
-#             for t in range(self.trial):
-#                 self.wandb_setup(self.full_N, t)
-#                 self.train_net(0, self.full_N, t)
-#                 wandb.finish()
-                
+
+    def train_loop2(self):
+        for iN, N in enumerate(self.Ns):
+            for t in range(self.trial):
+                self.train_net2(iN, N, t)
+
     def save_Ws(self, path2data):
         pd.to_pickle(self.eig_Ws, self.eig_path + '_Ws_{path2data}'.format(path2data=path2data))
         pd.to_pickle(self.eig_alphas, self.eig_path + '_alphas_{path2data}'.format(path2data=path2data))
-        
+
     def train_all(self):
         self.load_dataset()
         self.train_setup()
         self.train_loop()
         path_split = self.path2data.split('.')
-        
-        if self.L > 1:
-            self.save_Ws('.'.join(path_split[:-1]) + '{Ns}_deep'.format(Ns=self.Ns[0]) + path_split[-1])
-        elif self.L == 1: 
-            self.save_Ws('.'.join(path_split[:-1]) + '{Ns}_shallow'.format(Ns=self.Ns[0]) + path_split[-1])
-#         if self.deep:
-#             self.save_Ws('.'.join(path_split[:-1]) + '{Ns}_deep'.format(Ns=self.Ns[0]) + path_split[-1])
-#         else:
-#             self.save_Ws('.'.join(path_split[:-1]) + '{Ns}_shallow'.format(Ns=self.Ns[0]) + path_split[-1])
 
-    
+
 if __name__ == "__main__":
     fire.Fire(trainNet)
-    
 
-    
-    
-    
-    
-    
+
+
+
+
+
